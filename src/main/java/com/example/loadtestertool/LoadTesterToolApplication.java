9ea6d60045104c7daec3ef1283955aa62c52c429
Readthisname <gravewormed@gmail.com>
2024-05-03 19:38:43 +0300

    first commit

---
+++ src/main/java/com/example/loadtestertool/LoadTesterToolApplication.java
@@ -0,0 +1,14 @@
+package com.example.loadtestertool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadTesterToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadTesterToolApplication.class, args);
+	}
+
+	
+}
---
+++ src/test/java/com/example/loadtestertool/LoadTesterToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loadtestertool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadTesterToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
